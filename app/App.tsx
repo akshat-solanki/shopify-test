dda435d387d2e7c8edd7f5a56e43960e563a56b7
akshat solanki <akshat.solanki@ipsator.com>
2026-07-22 04:29:41 +0530

    commit 2

--- app/App.tsx
+++ app/App.tsx
@@ -1,1 +1,15 @@
-export { default } from "./pages/ProductShowcaseStudio";
+import ProductShowcaseStudio from "./pages/ProductShowcaseStudio";
+import ShopifyWorkspacePage from "./pages/ShopifyWorkspace";
+
+function hasShopifyContext() {
+  if (typeof window === "undefined") {
+    return false;
+  }
+
+  const search = new URLSearchParams(window.location.search);
+  return search.has("shop");
+}
+
+export default function App() {
+  return hasShopifyContext() ? <ShopifyWorkspacePage /> : <ProductShowcaseStudio />;
+}
